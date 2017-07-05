32306522b6066abd6fa0ec92ca58602b8202acc9
Young3276 <ba6020@naver.com>
2017-07-05 19:02:45 +0900

    170705_test

--- PRAC_C/stack/arithmetic.c
+++ PRAC_C/stack/arithmetic.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 
 struct Stack{
-	char ch;
+	char data;
 	struct Stack *next;
 }*top;
 
@@ -11,28 +11,69 @@ struct Stack *CreateStack(){
 	return NULL;
 }
 
-void Push(struct Stack **top){
-
+void Push(struct Stack **top, char data){
+	struct Stack *temp;
+	temp=(struct Stack*)malloc(sizeof(struct Stack));
+	if(!temp){
+		printf("Memory Error!\n");
+		return;
+	}
+	temp->data=data;
+	temp->next=*top;
+	*top=temp;	
 }
 
 char Pop(struct Stack **top){
 	char data;
+	struct Stack *temp;
+	temp=*top;
+	*top=temp->next;
+	data=temp->data;
+	free(temp);
 	return data;
 }
+/*
+char auto_pop(struct Stack **top){
+	struct Stack *temp;
+	while(*top){
+		temp=*top
+		printf("%c", temp->data);
+		*top=temp->next;
+		free
+	}
+}
+*/
 
 int main(int argc, char* argv[]){
-
 	if(argc!=2){
 		printf("usage : %s <arithmetic operation>\n", argv[0]);
 		exit(1);
 	}
+
 	struct Stack *S;
-	char ch;
+	char ch,data;
+	int flag; //연산자=1 & 피연산자=0 플래그
 	int i=0;
 	S=CreateStack();
-	while(ch=(*(*(argv+1)+i))!='\0'){
-		//
-		printf("argv[1][%d] : %c\n", i, argv[1][i]);
+	/* 받은 수식열 문자단위로 체크 */
+	while((ch=(*(*(argv+1)+i)))!='\0'){
+		printf("aaaaaaaaaaaaaaaaaa\n");
+		if(!(ch>=40 && ch<=47)){
+			printf("%c", ch);
+		}else if(ch==41){
+			while((data=Pop(&top))!=40){
+				if(data!=40)
+					printf("%c", data);
+			}
+		}else{
+			while(top!='\0'){
+				printf("cccc");
+				data=Pop(&top);
+				printf("%c", data);
+			}		
+			Push(&top, ch);
+		}
+	//	printf("argv[1][%d] : %c   %d\n", i, argv[1][i], ch);
 		i++;
 	}
 	return 0;
--- PRAC_C/stack/palindrome.c
+++ PRAC_C/stack/palindrome.c
@@ -2,13 +2,15 @@
 #include<string.h>
 #include<stdlib.h>
 
+/* Palindrome 확인 함수 */
 int IsPalindrome(char* sen){
 	int check;
 	int ini;
 	int len=strlen(sen)-1;
 	for(ini=0; ini<len && sen[ini]==sen[len]; ini++, len--)
 	{
-/*		printf("sen[%d] : %c\n", ini, sen[ini]);
+/*	
+		printf("sen[%d] : %c\n", ini, sen[ini]);
 		printf("sen[%d] : %c\n", len, sen[len]);
 		printf("===================\n");
 */
@@ -25,6 +27,7 @@ int IsPalindrome(char* sen){
 
 int main(int argc, char* argv[]){
 
+	/* 파라미터 개수 확인하는 부분 */
 	if(argc!=2){
 		printf("usage : %s <sentence>\n",argv[0]);
 	}
@@ -32,6 +35,7 @@ int main(int argc, char* argv[]){
 	int result;
 	result=IsPalindrome(argv[1]);
 
+	/* 결과값 출력 부분 */
 	if(result)
 		printf("Sentence is Palindrome\n");
 	else
